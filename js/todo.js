b53834bcadec8f5a4a594325ca75f677282239ba
Ivck <39829296+Ivck@users.noreply.github.com>
2018-06-23 11:09:19 +0300

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,43 @@ let input=document.querySelector('input');
 					let button=document.querySelector('button');
 					let ul=document.querySelector('ul');
 					let butn1=document.getElementById('butn1');
-					let i;
-					let mas=[];
 					
-					
-					button.addEventListener('click',function(){
+					var mas=[];
+					if (localStorage.getItem('task')!=undefined) {
+					mas=JSON.parse(localStorage.getItem('task'));
+						
 				
-					mas.push(input.value);
-					localStorage.setItem('task',mas);
+					for(var i=0;i!=mas.length;i++){
+					let div1=document.createElement('div');
+					document.body.appendChild(div1);
+					div1.className +='location'
+					let div=document.createElement('div');
+					ul.appendChild(div);
+					let inp= document.createElement("input");
+					
+					inp.type = "checkbox";
+					div.appendChild(inp);
+					let li=document.createElement('Li');
+	
+					li.innerHTML=mas[i];
+					li.style.display='inline-block'
+					div.appendChild(li);
+					butn1.addEventListener('click',function(){
+						if (inp.checked) {
+							div1.appendChild(inp);
+							div1.appendChild(li);
+							inp.disabled=true;
+
+					}	
+					});
+				
+					}
+				}
+					button.addEventListener('click',function (){
+					if (input.value!="") {mas.push(input.value);
 					
 					
+				
 					let div=document.createElement('div');
 					ul.appendChild(div);
 
@@ -30,43 +57,20 @@ let input=document.querySelector('input');
 					div.appendChild(li);
 
 					butn1.addEventListener('click',function(){
-						if (inp.checked) {
-							div1.appendChild(inp);
-							div1.appendChild(li);
-							inp.disabled=true;
-						
-					}	
-					});	
-					input.value=""
-					
-					});	
-					let newmas=localStorage.getItem('task').split(',');
-					for(var newi=0;newi!=newmas.length;newi++){
-					let div1=document.createElement('div');
-					document.body.appendChild(div1);
-					div1.className +='location'
-					let newdiv=document.createElement('div');
-					ul.appendChild(newdiv);
-					let newinp= document.createElement("input");
-					
-					newinp.type = "checkbox";
-					newdiv.appendChild(newinp);
-					let newli=document.createElement('Li');
-	
-					newli.innerHTML=newmas[newi];
-					newli.style.display='inline-block'
-					newdiv.appendChild(newli);
-						
-
-					butn1.addEventListener('click',function(){
-						if (newinp.checked) {
-							div1.appendChild(newinp);
-							div1.appendChild(newli);
-							newinp.disabled=true;
+					if (inp.checked) {
+					div1.appendChild(inp);
+					div1.appendChild(li);
+					inp.disabled=true;
 
 					}	
 					});
 
-				
+					localStorage.setItem('task',JSON.stringify(mas));
+					
+					input.value=""
 					}
+					
+					});	
+							
+					
 				
